0efd6d0a0eeecea414a1a50175d115ec2c7476dc
Anastasia5877 <issv757@gmail.com>
2024-09-23 20:08:14 +0500

    task01 is completed

---
+++ task01/task01/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Парус");
+
+            Console.WriteLine();
+
+            Console.ForegroundColor= ConsoleColor.Yellow;
+            Console.WriteLine("Белеет парус одинокой");
+            Console.WriteLine("В тумане моря голубом!..");
+            Console.WriteLine("Что ищет он в стране далекой?");
+            Console.WriteLine("Что кинул он в краю родном?..");
+
+            Console.ResetColor();
+            Console.ReadKey();
+        }
+    }
+}
