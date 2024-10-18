ee17785088394c6e75d9f230affadd133f756cff
Anastasia5877 <issv757@gmail.com>
2024-10-18 23:13:01 +0500

    task06(2) is completed

---
+++ task0607/task07/task07/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task07
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var s = "теплоход";
+            Console.WriteLine($"Из слова \"{s}\" получили");
+
+            var word1 = s
+                .Remove(0, 3)
+                .Substring(2, 2) +
+                s.Substring(3, 2) +
+                s.Substring(7, 1)
+
+                ;
+            Console.WriteLine(word1);
+
+
+            var word2 = s
+                .Substring(7, 1) +
+                s.Remove(0,1)
+                .Remove(1,1)
+                .Remove(3, 3)
+                ;
+
+            Console.WriteLine(word2);
+            Console.ReadKey();
+
+        }
+
+        static string ReverseString(string s)
+        {
+            return new string (s.Reverse().ToArray());
+        }
+    }
+}
