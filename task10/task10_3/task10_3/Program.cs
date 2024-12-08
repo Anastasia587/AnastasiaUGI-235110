c0f38514a4f521076bae2ad5f2dd75fbec1784ee
Anastasia5877 <issv757@gmail.com>
2024-12-08 19:36:49 +0500

    task10_3 is completed

---
+++ task10/task10_3/task10_3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task10_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+
+            Console.WriteLine("Введите натуральное число n:");
+            while (!int.TryParse(Console.ReadLine(), out n) || n <= 0)
+            {
+                Console.WriteLine("Ошибка, введите натуральное число большее 0:");
+            }
+
+            Console.WriteLine($"Квадраты натуральных чисел, не превышающие {n}:");
+
+            for (int i = 1; i * i <= n; i++)
+            {
+                Console.WriteLine($"{i}^2 = {i * i}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
